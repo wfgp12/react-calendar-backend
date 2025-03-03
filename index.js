284064b7ba4029e4b5a2e1c11fdead07d3e6924c
wfgp12 <w.gutierrez1206@gmail.com>
2025-03-02 22:14:02 -0500

    chore: configure Express and add public folder files
    Configured Express to handle the backend server. Additionally, added files in the public folder to serve static content.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hola mundo!!')
+const express = require("express")
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+})
